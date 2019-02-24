55181a388c3968062dd2a9e5426ce4c8cb408800
timick <timikm2002@gmail.com>
2019-02-24 22:06:13 +0500

    Initial commit

---
+++ app/src/main/java/com/timickb/olympiadnotifier/MainFragment.java
@@ -0,0 +1,21 @@
+package com.timickb.olympiadnotifier;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+public class MainFragment extends Fragment {
+
+    Button applyBtn;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        applyBtn = (Button) getView().findViewById(R.id.applyBtn);
+        applyBtn.
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+}
