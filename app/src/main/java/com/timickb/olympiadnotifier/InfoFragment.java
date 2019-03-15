7d5db6fc2e7748b1d1a4c9a3b0f54c0aaafec4f5
timick <timikm2002@gmail.com>
2019-03-15 22:10:03 +0500

    Some interface improvements

--- app/src/main/java/com/timickb/olympiadnotifier/InfoFragment.java
+++ app/src/main/java/com/timickb/olympiadnotifier/InfoFragment.java
@@ -4,15 +4,21 @@ import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.text.method.LinkMovementMethod;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 public class InfoFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_info, container, false);
+        View view = inflater.inflate(R.layout.fragment_info, container, false);
+        TextView feedbackLink = view.findViewById(R.id.feedback);
+        feedbackLink.setMovementMethod(LinkMovementMethod.getInstance());
+        TextView githubLink = view.findViewById(R.id.sourcesLink);
+        githubLink.setMovementMethod(LinkMovementMethod.getInstance());
+        return view;
     }
 }
